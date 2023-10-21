8d0a90753717446f4589bf4868c382b136f6a88e
nikolaoskalomiris <nikoskalomiris@gmail.com>
2023-10-21 23:29:29 +0300

    a23xq: recovery: Add firmware and modules to recovery

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -22,7 +22,17 @@ RECOVERY_KMOD_TARGETS := \
     sec_common_fn.ko \
     sec_secure_touch.ko \
     sec_tsp_dumpkey.ko \
-    sec_tsp_log.ko
+    sec_tsp_log.ko \
+    novatek_ts_nt36523.ko \
+    ovt_td4150_spi.ko \
+    lxs_ts.ko
+
+RECOVERY_FIRMWARE_TARGETS := \
+    lx891rs_a23xq_ltps.bin \
+    nt36672_a23_csot_mp.bin \
+    nt36672_a23_csot.bin \
+    td4375_a23xq_boe.bin \
+    td4375_a23xq_tianma.bin
 
 INSTALLED_RECOVERY_KMOD_TARGETS := $(RECOVERY_KMOD_TARGETS:%=$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/%)
 $(INSTALLED_RECOVERY_KMOD_TARGETS): $(INSTALLED_KERNEL_TARGET)
@@ -30,4 +40,10 @@ $(INSTALLED_RECOVERY_KMOD_TARGETS): $(INSTALLED_KERNEL_TARGET)
 	@mkdir -p $(dir $@)
 	cp $(@F:%=$(TARGET_OUT_VENDOR)/lib/modules/%) $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/
 
-$(recovery_uncompressed_ramdisk): $(INSTALLED_RECOVERY_KMOD_TARGETS)
+INSTALLED_RECOVERY_FIRMWARE_TARGETS := $(RECOVERY_FIRMWARE_TARGETS:%=$(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware/%)
+$(INSTALLED_RECOVERY_FIRMWARE_TARGETS): $(INSTALLED_KERNEL_TARGET)
+	echo -e ${CL_GRN}"Copying touchscreen firmware to recovery"${CL_RST}
+	@mkdir -p $(dir $@)
+	cp $(@F:%=$(TARGET_OUT_VENDOR)/firmware/%) $(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware/
+
+$(recovery_uncompressed_ramdisk): $(INSTALLED_RECOVERY_KMOD_TARGETS) $(INSTALLED_RECOVERY_FIRMWARE_TARGETS)
